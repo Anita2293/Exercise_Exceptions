e59704794c2fa6deb1c51581d028aa273be79a03
Anabella Bravo <anabellabravo93@hotmail.com>
2024-10-25 20:36:34 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] tours = { "England", "Spain", "Italy", "Portugal", "France" };
+            int choice = Convert.ToInt32(Console.ReadLine());
+            int N = choice;
+
+            //your code goes here
+            try
+            {
+                Console.WriteLine(tours[N]);
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Wrong number");
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye");
+            }
+        }
+    }
+}
